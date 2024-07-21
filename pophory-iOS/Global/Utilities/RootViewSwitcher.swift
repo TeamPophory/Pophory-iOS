635d1107d7e828449363b3f0e522f7cc7c40e9c6
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-07-22 01:29:03 +0900

    [Refactor] #279 - RootViewSwitcher 생성 - 중복 코드 제거 - 로그인 여부 확인하는 함수 추가

--- pophory-iOS/Global/Utilities/RootViewSwitcher.swift
+++ pophory-iOS/Global/Utilities/RootViewSwitcher.swift
@@ -11,6 +11,8 @@ enum RootView {
     case onboarding
     case home
     case albumFull
+    case addPhoto(image: UIImage, imageType: PhotoCellType)
+    case share(shareId: String)
 }
 
 final class RootViewSwitcher {
@@ -49,7 +51,21 @@ extension RootViewSwitcher {
                                          image: ImageLiterals.img_albumfull,
                                          primaryText: "포포리 앨범이 가득찼어요",
                                          secondaryText: "아쉽지만,\n다음 업데이트에서 만나요!")
+        case .share(let shareId):
+            rootViewController = ShareViewController()
+            if let vc = rootViewController as? ShareViewController {
+                vc.setupShareID(forShareID: shareId)
+            }
+        case .addPhoto(let image, let imageType):
+            let tabBarController = TabBarController()
+            let addPhotoViewController = AddPhotoViewController()
+            addPhotoViewController.setupRootViewImage(forImage: image, forType: imageType)
+            
+            let navigationController = PophoryNavigationController(rootViewController: tabBarController)
+            navigationController.pushViewController(addPhotoViewController, animated: false)
+            return
         }
         window.rootViewController = PophoryNavigationController(rootViewController: rootViewController)
+        window.makeKeyAndVisible()
     }
 }
